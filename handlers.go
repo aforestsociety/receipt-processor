988151df0f98a230e54d7e25683c326dd714cfd5
Forest Society <contacttheforestsociety@gmail.com>
2023-09-11 16:40:27 -0500

    implemented point calculation logic and tests

--- handlers.go
+++ handlers.go
@@ -4,7 +4,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"math"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
+	"unicode"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -93,7 +98,100 @@ func GetPoints(w http.ResponseWriter, r *http.Request) {
 }
 
 func calculatePoints(receipt Receipt) int64 {
-	return int64(100)
+	points := int64(0)
+
+	// Rule 1: One point for every alphanumeric character in the retailer name.
+	points += countAlphaNumeric(receipt.Retailer)
+	// Rule 2: 50 points if the total is a round dollar amount with no cents.
+	if isRoundDollarAmount(receipt.Total) {
+		points += 50
+	}
+	// Rule 3: 25 points if the total is a multiple of 0.25.
+	if isMultipleOf25Cents(receipt.Total) {
+		points += 25
+	}
+	// Rule 4: 5 points for every two items on the receipt.
+	points += int64(numItemsOnReceipt(receipt.Items)/2) * 5
+	// Rule 5: If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+	for _, item := range receipt.Items {
+		if isMultipleOf3(item.ShortDescription) {
+			points += int64(math.Ceil(stringToFloat(item.Price) * 0.2))
+		}
+	}
+	// Rule 6: 6 points if the day in the purchase date is odd.
+	if isOddDay(receipt.PurchaseDate) {
+		points += 6
+	}
+	// Rule 7: 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+	if isBetween2And4PM(receipt.PurchaseTime) {
+		points += 10
+	}
+
+	return points
+}
+
+func countAlphaNumeric(s string) int64 {
+	count := 0
+	for _, char := range s {
+		if unicode.IsLetter(char) || unicode.IsDigit(char) {
+			count++
+		}
+	}
+	return int64(count)
+}
+
+func isRoundDollarAmount(s string) bool {
+	// Clean and convert string
+	val := stringToFloat(s)
+	if val == -1 {
+		return false
+	}
+
+	// Compare float value to its int counterpart
+	return val == math.Trunc(val)
+}
+
+func isMultipleOf25Cents(s string) bool {
+	// Clean and convert string
+	val := stringToFloat(s)
+	if val == -1 {
+		return false
+	}
+
+	// Check if cleanly divisible by 0.25
+	return math.Mod(val, 0.25) == 0
+}
+
+func numItemsOnReceipt(items []Item) int {
+	return len(items)
+}
+
+func isMultipleOf3(s string) bool {
+	// Remove whitespace
+	length := len(strings.ReplaceAll(s, " ", ""))
+	if length > 0 {
+		return length%3 == 0
+	}
+
+	return false
+}
+
+func isOddDay(s string) bool {
+	day, err := getDayFromDate(s)
+	if err != nil {
+		return false
+	}
+
+	return day%2 != 0
+}
+
+func isBetween2And4PM(s string) bool {
+	parsedTime, _ := parseTime(s)
+
+	startTime, _ := parseTime("14:00")
+	endTime, _ := parseTime("16:00")
+
+	return parsedTime.After(startTime) && parsedTime.Before(endTime)
 }
 
 func setPoints(id string, points int64) string {
@@ -158,4 +256,27 @@ func validateReceipt(receipt Receipt) ErrorResponse {
 
 func generateUniqueID() string {
 	return uuid.NewString()
+}
+
+func stringToFloat(s string) float64 {
+	// Remove commas if they exist
+	s = strings.ReplaceAll(s, ",", "")
+
+	val, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return -1
+	}
+	return val
+}
+
+func getDayFromDate(s string) (int, error) {
+	parsedDate, err := time.Parse(time.DateOnly, s)
+	if err != nil {
+		return 0, err
+	}
+	return parsedDate.Day(), nil
+}
+
+func parseTime(s string) (time.Time, error) {
+	return time.Parse("15:04", s)
 }
--- handlers_test.go
+++ handlers_test.go
@@ -49,7 +49,6 @@ func TestGetPointsHandler(t *testing.T) {
 		},
 	}
 
-	// Check cases one by one
 	for _, testCase := range testCases {
 		t.Run(testCase.description, func(t *testing.T) {
 			if testCase.receiptID == "12345" {
@@ -86,6 +85,7 @@ func TestGetPointsHandler(t *testing.T) {
 }
 
 func TestPostReceiptHandler(t *testing.T) {
+	// Define slice of test cases
 	testCases := []struct {
 		description    string
 		requestBody    string
@@ -124,6 +124,272 @@ func TestPostReceiptHandler(t *testing.T) {
 			responeBody := recorder.Body.String()
 			if len(responeBody) == 0 {
 				t.Error("Expected non empty response body, got empty")
+			}
+		})
+	}
+}
+
+func TestCalculatePoints(t *testing.T) {
+	testCases := []struct {
+		description    string
+		receipt        Receipt
+		expectedPoints int64
+	}{
+		{
+			description: "Sample Receipt 1",
+			receipt: Receipt{
+				Retailer:     "Target",
+				PurchaseDate: "2022-01-02",
+				PurchaseTime: "11:11",
+				Items: []Item{
+					{ShortDescription: "Pepsi - 12-oz", Price: "1.25"},
+				},
+				Total: "1.25",
+			},
+			expectedPoints: int64(31),
+		},
+		{
+			description: "Sample Receipt 2",
+			receipt: Receipt{
+				Retailer:     "Walgreens",
+				PurchaseDate: "2022-01-21",
+				PurchaseTime: "08:13",
+				Items: []Item{
+					{ShortDescription: "Pepsi - 12-oz", Price: "1.25"},
+					{ShortDescription: "Dasani", Price: "1.40"},
+				},
+				Total: "2.65",
+			},
+			expectedPoints: int64(21),
+		},
+		{
+			description: "Sample Receipt 3",
+			receipt: Receipt{
+				Retailer:     "Record Store",
+				PurchaseDate: "2022-01-22",
+				PurchaseTime: "15:42",
+				Items: []Item{
+					{ShortDescription: "Blonde - Frank Ocean", Price: "24.99"},
+					{ShortDescription: "Volcano - Jungle", Price: "17.99"},
+					{ShortDescription: "Derealised - Jadu Heart", Price: "18.50"},
+					{ShortDescription: "Rare Pleasure - Mndsgn", Price: "22.99"},
+					{ShortDescription: "Maps - billy woods", Price: "19.99"},
+				},
+				Total: "104.46",
+			},
+			expectedPoints: int64(35),
+		},
+		{
+			description: "Sample Receipt 4",
+			receipt: Receipt{
+				Retailer:     "T's Candy",
+				PurchaseDate: "2022-01-09",
+				PurchaseTime: "16:00",
+				Items: []Item{
+					{ShortDescription: "Mambas", Price: "2.00"},
+					{ShortDescription: "Skittles", Price: "1.75"},
+					{ShortDescription: "Albanese Gummy Bears", Price: "3.25"},
+				},
+				Total: "7.00",
+			},
+			expectedPoints: int64(95),
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			points := calculatePoints(testCase.receipt)
+
+			if points != testCase.expectedPoints {
+				t.Errorf("Expected points: %d, got %d", testCase.expectedPoints, points)
+			}
+		})
+	}
+}
+
+func TestCountAlphaNumeric(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected int64
+	}{
+		{"Hello World!", 10}, // 10 alphanumeric characters
+		{"12345", 5},         // 5 numeric characters
+		{"ABCdef", 6},        // 6 letter characters
+		{"!@#$%^&*()_+", 0},  // No alphanumeric characters
+		{"", 0},              // Empty string
+		{"   ", 0},           // Only spaces, no alphanumeric characters
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.input, func(t *testing.T) {
+			count := countAlphaNumeric(testCase.input)
+			if count != testCase.expected {
+				t.Errorf("Expected %d alphanumeric characters, but got %d", testCase.expected, count)
+			}
+		})
+	}
+}
+
+func TestIsRoundDollarMount(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected bool
+	}{
+		{"0", true},
+		{"12.34", false},
+		{"5.0", true},
+		{"1,000,000.00", true},
+		{"0.01", false},
+		{"abc", false},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.input, func(t *testing.T) {
+			actual := isRoundDollarAmount(testCase.input)
+			if actual != testCase.expected {
+				t.Errorf("Expected isRoundDollarAmount(%s) to be %v, but got %v", testCase.input, testCase.expected, actual)
+			}
+		})
+	}
+}
+
+func TestIsMultipleOf25Cents(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected bool
+	}{
+		{"0.00", true},
+		{"0.25", true},
+		{"1.23", false},
+		{"1,000,000.75", true},
+		{"19.4", false},
+		{"abc", false},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.input, func(t *testing.T) {
+			actual := isMultipleOf25Cents(testCase.input)
+			if actual != testCase.expected {
+				t.Errorf("Expected isRoundDollarAmount(%s) to be %v, but got %v", testCase.input, testCase.expected, actual)
+			}
+		})
+	}
+}
+
+func TestNumItemsOnReceipt(t *testing.T) {
+	testCases := []struct {
+		description   string
+		items         []Item
+		expectedCount int
+	}{
+		{
+			description:   "No Items",
+			items:         []Item{},
+			expectedCount: 0,
+		},
+		{
+			description:   "1 Item",
+			items:         []Item{{ShortDescription: "Item 1", Price: "1.23"}},
+			expectedCount: 1,
+		},
+		{
+			description: "5 Items",
+			items: []Item{
+				{ShortDescription: "Item 1", Price: "1.23"},
+				{ShortDescription: "Item 2", Price: "1.23"},
+				{ShortDescription: "Item 3", Price: "1.23"},
+				{ShortDescription: "Item 4", Price: "1.23"},
+				{ShortDescription: "Item 5", Price: "1.23"}},
+			expectedCount: 5,
+		},
+		{
+			description: "8 Items",
+			items: []Item{
+				{ShortDescription: "Item 1", Price: "1.23"},
+				{ShortDescription: "Item 2", Price: "1.23"},
+				{ShortDescription: "Item 3", Price: "1.23"},
+				{ShortDescription: "Item 4", Price: "1.23"},
+				{ShortDescription: "Item 5", Price: "1.23"},
+				{ShortDescription: "Item 6", Price: "1.23"},
+				{ShortDescription: "Item 7", Price: "1.23"},
+				{ShortDescription: "Item 8", Price: "1.23"}},
+			expectedCount: 8,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			count := numItemsOnReceipt(testCase.items)
+			if count != testCase.expectedCount {
+				t.Errorf("Expected %d items, but got %d", testCase.expectedCount, count)
+			}
+		})
+	}
+}
+
+func TestIsMultipleOf3(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected bool
+	}{
+		{"", false},
+		{"abc", true},
+		{"abc 123", true},
+		{"sally sells sea shells", false},
+		{"! !  ! !", false},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.input, func(t *testing.T) {
+			actual := isMultipleOf3(testCase.input)
+			if actual != testCase.expected {
+				t.Errorf("Expected isMultipleOf3(%s) to be %v, but got %v", testCase.input, testCase.expected, actual)
+			}
+		})
+	}
+}
+
+func TestIsOddDay(t *testing.T) {
+	testCases := []struct {
+		purchaseDate string
+		expected     bool
+	}{
+		{"2022-01-01", true},  // January 1, odd
+		{"2023-02-14", false}, // February 14, even
+		{"2023-10-31", true},  // October 31, odd
+		{"2023-12-25", true},  // December 25, odd
+		{"2023-11-22", false}, // November 22 (my birthday)
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.purchaseDate, func(t *testing.T) {
+			actual := isOddDay(testCase.purchaseDate)
+			if actual != testCase.expected {
+				t.Errorf("Expected isOddDay(%s) to be %v, but got %v", testCase.purchaseDate, testCase.expected, actual)
+			}
+		})
+	}
+}
+
+func TestIsBetween2And4PM(t *testing.T) {
+	testCases := []struct {
+		purchaseTime string
+		expected     bool
+	}{
+		{"11:11", false}, // Before
+		{"14:00", false}, // 2 PM
+		{"14:01", true},  // Between
+		{"15:59", true},  // Between
+		{"16:00", false}, // 4 PM
+		{"16:01", false}, // After
+		{"20:12", false}, // After
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.purchaseTime, func(t *testing.T) {
+			actual := isBetween2And4PM(testCase.purchaseTime)
+			if actual != testCase.expected {
+				t.Errorf("Expected isBetween2And4PM(%s) to be %v, but got %v", testCase.purchaseTime, testCase.expected, actual)
 			}
 		})
 	}
